3df4044664647df01284626535f4fde077626106
w0nd3r0us <steven.grewal@gmail.com>
2018-06-13 15:37:31 -0400

    new picture function is refined

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,7 +13,19 @@ class PicturesController < ApplicationController
   end
 
   def create
-    render text: "Received POST request to '/pictures' with the date URL: #{params}"
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      #if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/pictures"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
   end
 
 end
