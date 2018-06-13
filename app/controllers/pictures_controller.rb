c36f4658fbce456c267e0a1f6413281bb414db24
w0nd3r0us <steven.grewal@gmail.com>
2018-06-13 15:31:50 -0400

    added function for new

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,5 +7,13 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
-  
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the date URL: #{params}"
+  end
+
 end
